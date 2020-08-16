b86200b39b1ec350427b168c3e92998c58f4b129
Deserie Murembeni <deserie.dev@gmail.com>
2020-08-17 01:02:00 +0200

    Styling and Navbar

--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -2,11 +2,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const GlossaryPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Cloud Blog</h1>    
+    <SEO title="Glossary" />
+    <h1>Cloud Glossary</h1>    
   </Layout>
 )
 
-export default IndexPage
+export default GlossaryPage
