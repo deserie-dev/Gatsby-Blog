ca7d0808890287c2f53ad29452892a875ef41490
Deserie Murembeni <deserie.dev@gmail.com>
2020-08-17 15:04:31 +0200

    Fixed Header Error

--- src/components/header.js
+++ src/components/header.js
@@ -9,33 +9,46 @@ import {
   Nav,
   NavItem,
   NavLink,
-  NavbarText
 } from 'reactstrap';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false,
+    }
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+  render() {
     return (
       <div>
         <Navbar fixed="top" light expand="sm">
-        <NavbarBrand href="/">{this.props.siteTitle}</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/about">About</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/glossary">Glossary</NavLink>
-            </NavItem>   
-            <NavItem>
-              <NavLink href="/tags">Tags</NavLink>
-            </NavItem>          
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
+          <NavbarBrand href="/">{this.props.siteTitle}</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/about">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/glossary">Glossary</NavLink>
+              </NavItem>   
+              <NavItem>
+                <NavLink href="/tags">Tags</NavLink>
+              </NavItem> 
+            </Nav>
+          </Collapse>    
         </Navbar>
-       </div>
-  );
-}
+      </div>  
+    );
+  }
+}  
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
